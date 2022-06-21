cb5a40871de98ec990a2ca0144a1d657f5e3c3a8
icho648 <38276516+icho648@users.noreply.github.com>
2022-06-21 15:45:22 +0800

    upd webpack_require

--- best-practices/webpack_require.js
+++ best-practices/webpack_require.js
@@ -62,3 +62,100 @@ var WebpackTest = (function (modules) {
 		}
 	},
 })
+
+/* src/a.js */
+export function funcA() {
+	console.log("in funcA")
+}
+
+/* src/index.js */
+import { funcA } from "./a"
+
+export function funcB() {
+	funcA()
+	console.log("in funcB")
+}
+/**
+ * @description 自执行函数，第一个return从Index文件开始，把定义的webpack_require传给index的模块函数，让它在内部替换import。
+ * 自执行函数的Modules参数被webpack_require闭包引用，webpack_require在遇到对应的id时直接去缓存再从modules里面找。
+ * installedModules缓存了id和对应的导出数据
+ */
+var WebpackTest2 = (function (modules) {
+	// webpackBootstrap
+	// The module cache
+	var installedModules = {}
+	// The require function
+	function __webpack_require__(moduleId) {
+		// Check if module is in cache
+		if (installedModules[moduleId]) {
+			return installedModules[moduleId].exports
+		}
+		// Create a new module (and put it into the cache)
+		var module = (installedModules[moduleId] = {
+			i: moduleId,
+			l: false,
+			exports: {},
+		})
+		// Execute the module function
+		modules[moduleId].call(
+			module.exports,
+			module,
+			module.exports,
+			__webpack_require__
+		)
+		// Flag the module as loaded
+		module.l = true
+		// Return the exports of the module
+		return module.exports
+	}
+	// define getter function for harmony exports
+	__webpack_require__.d = function (exports, name, getter) {
+		if (!__webpack_require__.o(exports, name)) {
+			Object.defineProperty(exports, name, { enumerable: true, get: getter })
+		}
+	}
+	// define __esModule on exports
+	__webpack_require__.r = function (exports) {
+		if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+			Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" })
+		}
+		Object.defineProperty(exports, "__esModule", { value: true })
+	}
+	// Object.prototype.hasOwnProperty.call
+	__webpack_require__.o = function (object, property) {
+		return Object.prototype.hasOwnProperty.call(object, property)
+	}
+
+	// Load entry module and return exports
+	return __webpack_require__((__webpack_require__.s = "./src/index.js"))
+})(
+	/************************************************************************/
+	{
+		"./src/a.js": function (module, __webpack_exports__, __webpack_require__) {
+			__webpack_require__.r(__webpack_exports__)
+			__webpack_require__.d(__webpack_exports__, "funcA", function () {
+				return funcA
+			})
+			function funcA() {
+				console.log("in funcA")
+			}
+		},
+
+		"./src/index.js": function (
+			module,
+			__webpack_exports__,
+			__webpack_require__
+		) {
+			__webpack_require__.r(__webpack_exports__)
+			__webpack_require__.d(__webpack_exports__, "funcB", function () {
+				return funcB
+			})
+			//在这里把import改写
+			var _a__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./src/a.js")
+			function funcB() {
+				Object(_a__WEBPACK_IMPORTED_MODULE_0__["funcA"])()
+				console.log("in funcB")
+			}
+		},
+	}
+)
